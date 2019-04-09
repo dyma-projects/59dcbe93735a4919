97acd82f44901860b1d475e4da09a36b023a62e9
florian.cerda@richemont.com <florian.cerda@richemont.com>
2019-04-09 08:27:38 +0200

    Projet 2

--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-exercice2',
   templateUrl: './exercice2.component.html',
-  styleUrls: ['./exercice2.component.css']
+  styleUrls: ['./exercice2.component.scss']
 })
 export class Exercice2Component implements OnInit {
-
+  
   constructor() { }
 
-  ngOnInit() {
+
+  displayConsole(): void {
+    console.log('Salut');
   }
+  
+  ngOnInit() {}
 
 }
---
+++ src/app/exercice3/exercice3.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercice3',
+  templateUrl: './exercice3.component.html',
+  styleUrls: ['./exercice3.component.scss']
+})
+export class Exercice3Component implements OnInit {
+
+  status:Boolean = true;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
